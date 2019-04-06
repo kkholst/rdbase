20221c7c7fb0f05016a21e22b35d6e2b5d7f9901
Klaus Holst <klaus@holst.it>
2019-04-06 15:51:20 +0200

    mlpack images

--- Dockerfile.r
+++ Dockerfile.r
@@ -3,59 +3,63 @@ FROM alpine:3.9
 
 MAINTAINER "Klaus Kähler Holst" klaus@holst.it
 
+ENV ARMA_BRANCH=9.300.x
 ENV R_BASE_VERSION 3.5.3
-
-ENV LC_ALL en_US.UTF-8
-ENV LANG en_US.UTF-8
+ARG USE_HDF5=OFF
 
 ENV BUILD_DEPS \
 	perl \
 	cairo-dev \
-	bzip2-dev \
 	curl-dev \
 	readline-dev \
-	xz-dev \
-	zlib-dev \
 	libpng-dev \
 	openssl-dev \
 	libxml2-dev \
-	pango-dev
-
-ENV PERSISTENT_DEPS \
-	ca-certificates \
-	pcre-dev \
-	libexecinfo-dev \
+	bzip2-dev \
+	zlib-dev \
+	xz-dev \
+	pango-dev \
+	cmake \
+	ninja \
 	curl \
-	perl \
+	git \
+	ncurses-terminfo ncurses \
 	libintl \
-	icu-libs \
-	libc-dev \
 	autoconf \
 	automake \
+	perl \
+	cmake \
+	python3
+	
+ENV PERSISTENT_DEPS \
+	libbz2 xz-libs \
+	pcre-dev \
+	libexecinfo-dev \
+	libcurl \
 	make \
-	git \
-	gcc \
 	g++ \
 	musl-dev \
 	gfortran \
-	openblas-dev \
 	coreutils \
-	bash ncurses \
-	python3
+	bash
+
+ENV LC_ALL en_US.UTF-8
+ENV LANG en_US.UTF-8
+ENV LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib64:/usr/lib64:${LD_LIBRARY_PATH}
 
 RUN	apk add --no-cache --virtual .build-deps $BUILD_DEPS && \
 	apk add --no-cache --virtual .persistent-deps $PERSISTENT_DEPS && \
-	python3 -m ensurepip && \
-	rm -r /usr/lib/python*/ensurepip && \
-	pip3 install --upgrade pip setuptools && \
-	if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
-	if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
-	mkdir /tmp/build_r && cd /tmp/build_r \
-	&& apk add --no-cache curl \
-	&& curl -sSLO https://cran.rstudio.com/src/base/R-${R_BASE_VERSION:0:1}/R-${R_BASE_VERSION}.tar.gz \
+	if [ "$USE_HDF5" = "ON" ]; then (apk add --no-cache \
+	--repository  http://dl-3.alpinelinux.org/alpine/edge/testing \
+	hdf5 hdf5-dev); fi && \
+	mkdir /tmp/build_r && cd /tmp/build_r && \
+	curl -sSLO https://cran.rstudio.com/src/base/R-${R_BASE_VERSION:0:1}/R-${R_BASE_VERSION}.tar.gz \
 	&& tar xf R-${R_BASE_VERSION}.tar.gz && cd R-${R_BASE_VERSION} \
-	&& ./configure --build=$CBUILD --host=$CHOST --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --localstatedir=/var \	
+	&& ./configure --build=$CBUILD --host=$CHOST --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --localstatedir=/var \
+	--enable-R-shlib \
+	--enable-BLAS-shlib \
 	--without-x \
+	--with-ICU=no \
 	--with-tcltk=no \
 	--with-aqua=no \
 	--without-recommended-packages \
@@ -63,15 +67,20 @@ RUN	apk add --no-cache --virtual .build-deps $BUILD_DEPS && \
 	--with-jpeglib=no --with-libtiff=no \
 	&& make -j $(cat /proc/self/status | awk '$1 == "Cpus_allowed_list:" { print $2 }' | tr , '\n' | awk -F'-' '{ if (NF == 2) count += $2 - $1 + 1; else count += 1 } END { print count }') \
 	&& make install && \
-	echo 'options("repos"="https://cran.rstudio.com")' >> /usr/lib/R/etc/Rprofile.site && \
+	echo 'options("repos"="https://cloud.r-project.org/")' >> /usr/lib/R/etc/Rprofile.site && \
 	echo 'CXXFLAGS  += -D__MUSL__' >> /usr/lib/R/etc/Makeconf && \
 	echo 'CXX1XFLAGS  += -D__MUSL__' >> /usr/lib/R/etc/Makeconf && \
+	echo 'CXX11FLAGS  += -D__MUSL__' >> /usr/lib/R/etc/Makeconf && \
 	cd src/nmath/standalone && \
 	make && \
 	make install && \
-	rm -rf /tmp/build_r && \
-	rm -r /root/.cache && \
-	apk del .build-deps
+	cd /tmp; git clone https://gitlab.com/conradsnicta/armadillo-code -b ${ARMA_BRANCH} --depth=1 armadillo && cd armadillo; \
+	cmake -G Ninja -D DETECT_HDF5=${USE_HDF5} ./ && \
+	ninja && ninja install && cd /tmp; rm -Rf /tmp/armadillo && \
+	apk del --no-cache .build-deps && \ 
+	if [ "$USE_HDF5" = "ON" ]; then (apk del --no-cache hdf5-dev); fi && \
+	rm -Rf /tmp/* /root/.cache /var/cache/apk/* 
+
 
 VOLUME /data
 WORKDIR /data
--- Makefile
+++ Makefile
@@ -1,17 +1,35 @@
 DOCKER=docker
 DOCKER_OPT=--network=host
-IMG=rimg
+IMG=test
 CMD=bash
 USER=kkholst
 REPO=rdbase
 TAG=$(IMG)
+PWD:=`pwd`
+CONTAINER_EXISTS:=$(shell $(DOCKER) ps | awk '{print $$NF}' | grep -w run_$(IMG))
 
+.PHONY: build
 build:
-	@$(DOCKER) build $(DOCKER_OPT) -f Dockerfile.$(IMG) -t $(TAG) .
+	@$(DOCKER) build --rm $(DOCKER_OPT) -f Dockerfile.$(IMG) -t $(TAG) .
 
+
+.PHONY: update
+update:
+	$(DOCKER) images --format "{{.Repository}}:{{.Tag}}" | grep -v "<none>" | sort | uniq | xargs -L1 docker pull
+
+.PHONY: run
 run:
-	@$(DOCKER) run $(DOCKER_OPT) -ti -t $(IMG) bash
+	@if [ -z "${CONTAINER_EXISTS}" ]; then \
+	$(DOCKER) run $(DOCKER_OPT) -v $(PWD)/data:/data --rm --privileged --name run_$(IMG) -ti -t $(IMG) $(CMD); \
+	else \
+	$(DOCKER) exec -ti run_$(IMG) $(CMD); \
+	fi;
 
+.PHONY: apkinfo
+apkinfo: 
+	$(DOCKER) run $(DOCKER_OPT) -ti -t $(IMG) apk info | xargs -n1 -I{} apk info -s {} | xargs -n4 | awk '{print $4,$1}' | sort -n
+
+.PHONY: apkinfo
 xrun:
 	xhost +SI:localuser:$$(id -un) &> /dev/null && \
 	   $(DOCKER) run $(DOCKER_OPT) -it --rm -e DISPLAY=$(DISPLAY) \
@@ -22,16 +40,30 @@ xrun:
            $(IMG) $(CMD)
 #	    --user $$(id -u):$$(id -g) \
 
-dclean:
-	@$(DOCKER) ps -a -q -f status=exited | xargs $(DOCKER) rm -v
+.PHONY: clean
+clean: dclean iclean
+	$(DOCKER) images
 
+.PHONY: dclean
+dclean:
+	-@$(DOCKER) ps -a -q -f status=exited | xargs $(DOCKER) rm -v
+
+.PHONY: iclean
 iclean:
 	-@$(DOCKER) rmi $(docker images --filter "dangling=true" -q --no-trunc)
-	@$(DOCKER) images -a | grep "^<none>" | awk '{print $$3}' | xargs $(DOCKER) rmi
+	-@$(DOCKER) images -a | grep "^<none>" | awk '{print $$3}' | xargs $(DOCKER) rmi
 
+.PHONY: login
+login:
+	@$(DOCKER) login --username=$(USER)
+
+.PHONY: dlogin
 dlogin:
 	@$(DOCKER) login --username=$(USER) --password=$(PWD)
 
+.PHONY: dpush
 dpush:
-	$(DOCKER) build $(DOCKER_OPT) -t $(USER)/$(REPO):$(TAG) . -f Dockerfile.$(IMG)
+	$(DOCKER) build --rm $(DOCKER_OPT) -t $(USER)/$(REPO):$(TAG) . -f Dockerfile.$(IMG)
 	$(DOCKER) push $(USER)/$(REPO):$(TAG)
+
+
